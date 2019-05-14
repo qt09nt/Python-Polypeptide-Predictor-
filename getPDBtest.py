51b8127606bd1678cc2108179dd5e70915ca9233
qt09nt <45493336+qt09nt@users.noreply.github.com>
2019-05-14 12:36:46 -0400

    Delete getPDBtest.py

--- getPDBtest.py
+++
@@ -1,7 +0,0 @@
-import os
-import requests as re
-
-def fetchpdb(pdb,dir):
-    with open(os.join(dir,str(pdb)+'.pdb'),mode='wb') as f:
-        resp = re.get('https://files.rcsb.org/download/'+str(pdb))
-        f.write(resp.content)
