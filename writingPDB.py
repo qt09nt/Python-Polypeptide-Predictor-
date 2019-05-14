b7cefd0f9a924ab426f0df4305e0b1c8b028056c
qt09nt <45493336+qt09nt@users.noreply.github.com>
2019-05-14 12:38:21 -0400

    Delete writingPDB.py

--- writingPDB.py
+++
@@ -1,7 +0,0 @@
-c = open("rs126PDB.txt", "w")
-pdbName = []
-
-with open("rs126NoBlanks.txt") as f:
-    for line in f:
-        pdbName = (str(pdbName).join(".pdb") + "/n")  
-        c.write(line)
