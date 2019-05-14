5e8bac4acf2196148049606ed04abc2cf1dbf8ac
qt09nt <45493336+qt09nt@users.noreply.github.com>
2019-05-14 12:36:58 -0400

    Delete retrieveRS126FileNames.py

--- retrieveRS126FileNames.py
+++
@@ -1,9 +0,0 @@
-#Retrieve file names from rs126 folder and write into a text file
-
-import os
-
-a = open("rs126FileList.txt", "w")
-for path, subdirs, files in os.walk(r'C:\Users\sunflower\Desktop\python protein project\rs126'):
-   for filename in files:
-     f = os.path.join(path, filename)
-     a.write(str(f) + os.linesep) 
